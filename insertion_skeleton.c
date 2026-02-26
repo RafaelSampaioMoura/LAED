bd1ac0233613b59b8f0ad560d909934128e4396a
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2026-02-26 11:22:18 -0300

    Improved Insertion Sort

--- insertion_skeleton.c
+++ insertion_skeleton.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct
 {
@@ -8,6 +9,28 @@ typedef struct
     long swaps;
 } stats;
 
+void imprimeVetor(int v[], int n);
+
+void insertionSort(int v[], int n, stats *stats, bool order);
+
+void swp_int(int *a, int *b);
+
+int main(int argc, char const *argv[])
+{
+    stats s = {0, 0};
+    /* code */
+    int A[] = {5, 2, 4, 6, 1, 3};
+    int B[] = {1, 2, 3, 4, 5, 6};
+    int C[] = {6, 5, 4, 3, 2, 1};
+    insertionSort(A, 6, &s, false);
+    imprimeVetor(A, 6);
+    insertionSort(B, 6, &s, true);
+    imprimeVetor(B, 6);
+    insertionSort(C, 6, &s, false);
+    imprimeVetor(C, 6);
+    return 0;
+}
+
 void swp_int(int *a, int *b)
 {
     int tmp = *a;
@@ -15,7 +38,7 @@ void swp_int(int *a, int *b)
     *b = tmp;
 }
 
-void insertionSort(int v[], int n)
+void insertionSort(int v[], int n, stats *stats, bool order)
 {
     if (!v || n == 1)
     {
@@ -27,13 +50,30 @@ void insertionSort(int v[], int n)
     for (i = 0; i < n - 1; i++)
     {
         /* code */
+        bool wasSwpd = false;
         for (j = i; j > -1; j--)
         {
             /* code */
-
-            if (v[j] > v[j + 1])
+            //printf("I:%d, chave: %d, ", j, v[j]);
+            //imprimeVetor(v, 6);
+            stats->comparisons++;
+            if (order)
             {
-                swp_int(&v[j], &v[j + 1]);
+                if (v[j] > v[j + 1])
+                {
+                    stats->swaps++;
+                    swp_int(&v[j], &v[j + 1]);
+                }
+                //imprimeVetor(v, 6);
+            }
+            else
+            {
+                if (v[j] < v[j + 1])
+                {
+                    stats->swaps++;
+                    swp_int(&v[j+1], &v[j]);
+                }
+                //imprimeVetor(v, 6);
             }
         }
     }
@@ -51,19 +91,4 @@ void imprimeVetor(int v[], int n)
             printf(", ");
     }
     printf("]\n");
-}
-
-int main(int argc, char const *argv[])
-{
-    /* code */
-    int A[] = {5, 2, 4, 6, 1, 3};
-    int B[] = {1, 2, 3, 4, 5, 6};
-    int C[] = {6, 5, 4, 3, 2, 1};
-    insertionSort(A, 6);
-    imprimeVetor(A, 6);
-    insertionSort(B, 6);
-    imprimeVetor(B, 6);
-    insertionSort(C, 6);
-    imprimeVetor(C, 6);
-    return 0;
 }
