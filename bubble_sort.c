47074a1c3749551e2e8b8d9f5c8ead4b9594dfdb
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2026-03-16 15:42:44 -0300

    3. early stop

--- bubble_sort.c
+++ bubble_sort.c
@@ -12,9 +12,13 @@ typedef struct
 } stats;
 
 
-void swp_int (int *a, int *b, stats *s);
+void swp_int_stats (int *a, int *b, stats *s);
+
+void swp_int (int *a, int *b);
 
 void bubble_sort_basic(int arr[], int n, stats* stats);
+
+void bubble_sort_improved(int arr[], int n, stats *stats);
 
 void imprimirVetor(int v[], int n);
 
@@ -22,7 +26,10 @@ int main(int argc, char const *argv[])
 {
     /* code */
     stats s = {0, 0};
+    stats s_two = {0, 0};
+
     int art[] = {23, 65, 12, 34, 56, 78, 32, 12, 45, 23, 56};
+    int art_two[] = {23, 65, 12, 34, 56, 78, 32, 12, 45, 23, 56};
     int n = sizeof(art)/ sizeof(art[0]);
     imprimirVetor(art, n);
     bubble_sort_basic(art, n, &s);
@@ -30,18 +37,32 @@ int main(int argc, char const *argv[])
     printf("Comparações: %ld\n", s.comps);
     printf("Mudanças: %ld\n", s.swaps);
 
+    imprimirVetor(art_two, n);
+    bubble_sort_improved(art_two, n, &s_two);
+    imprimirVetor(art_two, n);
+    printf("Comparações: %ld\n", s_two.comps);
+    printf("Mudanças: %ld\n", s_two.swaps);
+
     return 0;
 }
 
 
-void swp_int (int *a, int *b, stats *s)
+void swp_int (int *a, int *b)
+{
+    int tmp;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void swp_int_stats (int *a, int *b, stats *s)
 {
     int tmp;
     tmp = *a;
     *a = *b;
     *b = tmp;
     s->swaps++;
-}
+};
 
 void bubble_sort_basic(int arr[], int n, stats *stats)
 {
@@ -53,9 +74,36 @@ void bubble_sort_basic(int arr[], int n, stats *stats)
         {
             if (arr[i] > arr[j])
             {
-                swp_int(&arr[i], &arr[j], stats);
+                swp_int_stats(&arr[i], &arr[j], stats);
             }
             stats->comps++;
+        }
+    }
+}
+
+void bubble_sort_improved(int arr[], int n, stats *stats)
+{
+    bool swapped;
+    int i, j;
+
+    for(int i = 0; i < n; i++)
+    {
+        //Isso melhora a performance do código por que, quando não houver mais nenhuma troca,
+        //não é preciso continuar atravessando o vetor, então o algoritmo finaliza cedo.
+        swapped = false;
+        for(j = i; j < n; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                swp_int_stats(&arr[i], &arr[j], stats);
+                swapped = true;
+            }
+            stats->comps++;
+        }
+
+        if(!swapped)
+        {
+            break;
         }
     }
 }
