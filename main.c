ed7ee9b0160205403ee3d56454e44c7e2e3d5671
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2026-03-17 20:13:03 -0300

    Coloca todas as funções de sort para um mesmo arquivo source

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <header.h>
-#include "insertion_sort.c"
-#include "selection_sort.c"
-#include "bubble_sort.c"
 #include "tests.c"
 #include "utils.c"
+#include "sorting_algo.c"
 
 
 int main(int argc, char const *argv[])
