55a6605ab29888dc6bd66fa1d1cb537e8e6941a8
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2026-03-17 10:32:36 -0300

    Insertion Sort

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,157 +1,73 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct
+void swp_int(int *a, int *b);
+
+void insertion_sort(int arr[], int n);
+
+void imprimir_vetor(int arr[], int n)
 {
-    /* data */
-    long comps;
-    long swaps;
-} stats;
-
-void swp_int(int *a, int *b, stats *s);
-
-void selection_sort(int arr[], int n, stats *s, int ordem);
-
-void imprimir_vetor(int v[], int n);
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+}
 
 int main(int argc, char const *argv[])
 {
-    int arr_one[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    int arr_two[20] = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int arr_three[20] = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int arr_four[20];
-    int arr_five[20];
+    /* code */
+    int N = 100;
 
-    int N = 100, i;
+    int arr[20];
 
-    for (i = 0; i < 20; i++)
+    for (int i = 0; i < 20; i++)
     {
+        /* code */
         int value = rand() % (N + 1);
-        arr_four[i] = value;
-        arr_five[i] = value;
+        arr[i] = value;
     }
+    printf("Inicial: ");
+    imprimir_vetor(arr, 20);
+    insertion_sort(arr, 20);
+    printf("Final: ");
+    imprimir_vetor(arr, 20);
 
-    stats s_one = {0, 0};
-    stats s_two = {0, 0};
-    stats s_three = {0, 0};
-    stats s_four = {0, 0};
-    stats s_five = {0, 0};
-    stats s_six = {0, 0};
-
-    printf("Array: Organizado, SelectionSort Crescente\n");
-    imprimir_vetor(arr_one, (sizeof(arr_one) / sizeof(arr_one[0])));
-    selection_sort(arr_one, (sizeof(arr_one) / sizeof(arr_one[0])), &s_one, 1);
-    imprimir_vetor(arr_one, (sizeof(arr_one) / sizeof(arr_one[0])));
-    printf("Comparações: %ld\n", s_one.comps);
-    printf("Mudanças: %ld\n", s_one.swaps);
-
-    printf("Array: Organizado, SelectionSort Decrescente\n");
-    imprimir_vetor(arr_one, (sizeof(arr_one) / sizeof(arr_one[0])));
-    selection_sort(arr_one, (sizeof(arr_one) / sizeof(arr_one[0])), &s_two, -1);
-    imprimir_vetor(arr_one, (sizeof(arr_one) / sizeof(arr_one[0])));
-    printf("Comparações: %ld\n", s_two.comps);
-    printf("Mudanças: %ld\n", s_two.swaps);
-
-    printf("Array: Invertido, SelectionSort Crescente\n");
-    imprimir_vetor(arr_two, (sizeof(arr_two) / sizeof(arr_two[0])));
-    selection_sort(arr_two, (sizeof(arr_two) / sizeof(arr_two[0])), &s_three, 1);
-    imprimir_vetor(arr_two, (sizeof(arr_two) / sizeof(arr_two[0])));
-    printf("Comparações: %ld\n", s_three.comps);
-    printf("Mudanças: %ld\n", s_three.swaps);
-
-    printf("Array: Invertido, SelectionSort Decrescente\n");
-    imprimir_vetor(arr_three, (sizeof(arr_three) / sizeof(arr_three[0])));
-    selection_sort(arr_three, (sizeof(arr_three) / sizeof(arr_three[0])), &s_four, -1);
-    imprimir_vetor(arr_three, (sizeof(arr_three) / sizeof(arr_three[0])));
-    printf("Comparações: %ld\n", s_four.comps);
-    printf("Mudanças: %ld\n", s_four.swaps);
-
-    printf("Array: Aleatório, SelectionSort Crescente\n");
-    imprimir_vetor(arr_four, (sizeof(arr_four) / sizeof(arr_four[0])));
-    selection_sort(arr_four, (sizeof(arr_four) / sizeof(arr_four[0])), &s_five, 1);
-    imprimir_vetor(arr_four, (sizeof(arr_four) / sizeof(arr_four[0])));
-    printf("Comparações: %ld\n", s_five.comps);
-    printf("Mudanças: %ld\n", s_five.swaps);
-
-    printf("Array: Aleatório, SelectionSort Decrescente\n");
-    imprimir_vetor(arr_five, (sizeof(arr_five) / sizeof(arr_five[0])));
-    selection_sort(arr_five, (sizeof(arr_five) / sizeof(arr_five[0])), &s_six, -1);
-    imprimir_vetor(arr_five, (sizeof(arr_five) / sizeof(arr_five[0])));
-    printf("Comparações: %ld\n", s_six.comps);
-    printf("Mudanças: %ld\n", s_six.swaps);
-    return 0;
-
+    
     return 0;
 }
 
-void swp_int(int *a, int *b, stats *s)
+void swp_int(int *a, int *b)
 {
     int tmp;
     tmp = *a;
     *a = *b;
     *b = tmp;
-    s->swaps++;
 }
 
-void selection_sort(int arr[], int n, stats *s, int ordem)
+void insertion_sort(int arr[], int n)
 {
-    int i, j, swapped = 0;
-    if (ordem == 1)
-    {
-        int smallest = 0;
-        for (i = 0; i < n; i++)
-        {
-            swapped = 0;
-            smallest = i;
-            printf("Intermediário: ");
-            imprimir_vetor(arr, n);
-            for (j = i; j < n; j++)
-            {
-                if (arr[j] < arr[smallest])
-                {
-                    smallest = j;
-                    s->comps++;
-                };
-            }
-            if (smallest != i)
-            {
-                swp_int(&arr[i], &arr[smallest], s);
-                swapped = 1;
-            }
-        }
-    }
-    if (ordem == -1)
-    {
-        int biggest = 0;
+    int i, j, key;
 
-        for (i = 0; i < n; i++)
-        {
-            biggest = i;
-            swapped = 0;
-            printf("Intermediário: ");
-            imprimir_vetor(arr, n);
-            for (j = i; j < n; j++)
-            {
-                if (arr[j] > arr[biggest])
-                {
-                    biggest = j;
-                    s->comps++;
-                };
-            }
-            if (biggest != i)
-            {
-                swp_int(&arr[i], &arr[biggest], s);
-                swapped = 1;
-            }
-        }
-    }
-};
-
-void imprimir_vetor(int v[], int n)
-{
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        printf("%d ", v[i]);
-    };
-    printf("\n");
+        /* code */
+        key = arr[i];
+        j = i -1;
+        
+        while (j >= 0 && arr[j] > key)
+        {
+            /* code */
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        };
+        arr[j + 1] = key;
+        printf("Intermediário: ");
+        imprimir_vetor(arr, n);
+        
+        
+    }
+    
 }
