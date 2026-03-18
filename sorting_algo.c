38b91fdaf740fab861f820cb6de6347c7cc2afb6
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2026-03-17 21:55:18 -0300

    PS

--- sorting_algo.c
+++ sorting_algo.c
@@ -91,13 +91,13 @@ void insertion_sort(int arr[], int n, stats *s)
     {
         /* code */
         key = arr[i];
-        j = i -1;
         
         while (j >= 0 && arr[j] > key)
         {
             /* code */
             arr[j + 1] = arr[j];
             j = j - 1;
+            s->swaps++;
             s->comps++;
         };
         arr[j + 1] = key;
